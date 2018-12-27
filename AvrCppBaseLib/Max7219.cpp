78ce8dc7648bae998a5c966479e66bd2da145ebd
Gergo Huszty <gergo.huszty@digitaltrip.hu>
2018-12-27 23:53:57 +0100

    MAX7219 display driver added with some TODOs.
    Thanks for adnbr (https://adnbr.co.uk/articles/max7219-and-7-segment-displays)

---
+++ AvrCppBaseLib/Max7219.cpp
@@ -0,0 +1,150 @@
+/* MAX7219 Interaction Code
+ * ---------------------------
+ * For more information see
+ * http://www.adnbr.co.uk/articles/max7219-and-7-segment-displays
+ *
+ * Changed to C++ by Gergo Huszty
+ */
+
+// Outputs, pin definitions
+#define PIN_SCK                   PORTB5
+#define PIN_MOSI                  PORTB3
+
+#define ON                        1
+#define OFF                       0
+
+#define MAX7219_MODE_DECODE       0x09
+#define MAX7219_MODE_INTENSITY    0x0A
+#define MAX7219_MODE_SCAN_LIMIT   0x0B
+#define MAX7219_MODE_POWER        0x0C
+#define MAX7219_MODE_TEST         0x0F
+#define MAX7219_MODE_NOOP         0x00
+
+// I only have 3 digits, no point having the
+// rest. You could use more though.
+#define MAX7219_DIGIT0            0x01
+#define MAX7219_DIGIT1            0x02
+#define MAX7219_DIGIT2            0x03
+
+#define MAX7219_CHAR_BLANK        0xF 
+#define MAX7219_CHAR_NEGATIVE     0xA 
+
+#include <avr/io.h>
+#include "Max7219.h"
+
+Max7219::Max7219(uint8_t newDigitsInUse,
+                 uint8_t newSsPin,
+                 uint8_t newRefreshTime): SoftTimerHandler(false, false, true),
+                                          digitsInUse(newDigitsInUse), SsPin(newSsPin), refreshTime(newRefreshTime),
+                                          number(0), prevNumber(0), dotPlace(0) {
+    myTimer.set(refreshTime);
+    init();
+}
+
+void Max7219::handleTimeout() {
+  if(prevNumber != number) {
+    applyContent();
+  }    
+  myTimer.set(refreshTime);
+}
+
+void Max7219::setNumber(int32_t newNumber, uint8_t newDotPlace) {
+  number = newNumber;
+  dotPlace = newDotPlace;
+}
+
+void Max7219::spiSendByte(char databyte) {
+    // Copy data into the SPI data register
+    SPDR = databyte;
+    // Wait until transfer is complete
+    while (!(SPSR & (1 << SPIF)));
+}
+
+void Max7219::writeData(char data_register, char data) {  
+  PORTB &= ~(1<<SsPin);
+  // Send the register where the data will be stored
+  spiSendByte(data_register);
+  // Send the data to be stored
+  spiSendByte(data);
+  PORTB |= (1<<SsPin);
+}
+
+void Max7219::clearDisplay() {
+	char i = digitsInUse;
+	// Loop until 0, but don't run for zero
+	do {
+		// Set each display in use to blank
+		writeData(i, MAX7219_CHAR_BLANK);
+	} while (--i);
+}
+
+/*void MAX7219_clearDisplayRaw()
+{
+	char i = digitsInUse;
+	// Loop until 0, but don't run for zero
+	do {
+		// Set each display in use to blank
+		MAX7219_writeData(i, 0);
+	} while (--i);
+}*/
+
+void Max7219::applyContent() {
+    char negative = 0;
+
+    prevNumber = number;
+    int32_t tempNumber = number;
+    if (tempNumber < 0) {
+        negative = 1;
+        tempNumber *= -1;
+    }
+
+    char i = 0; 
+    do {
+		  uint8_t data = tempNumber % 10;
+		  if (i == dotPlace) {
+			  data |= (1<<7);
+		  }
+      writeData(++i, data);
+      tempNumber /= 10;
+    } while (tempNumber);
+
+    if (negative) {
+        writeData(++i, MAX7219_CHAR_NEGATIVE);
+    }
+    do {
+	    writeData(++i, MAX7219_CHAR_BLANK);
+    } while (i<digitsInUse);
+}
+
+/*unsigned const char g_num_faces[11] = {
+	//    abcdefg
+	0b1111110,
+	0b0110000,
+	0b1101101,
+	0b1111001,
+	0b0110011,
+	0b1011011,
+	0b1011111,
+	0b1110000,
+	0b1111111,
+	0b1111011,
+	0b11000000};*/
+
+void Max7219::init() {
+	// SCK MOSI CS/LOAD/SS
+  DDRB |= (1 << PIN_SCK) | (1 << PIN_MOSI) | (1 << SsPin);
+
+  // SPI Enable, Master mode
+  SPCR |= (1 << SPE) | (1 << MSTR)| (1<<SPR1);
+
+  // Decode mode to "Font Code-B"
+  writeData(MAX7219_MODE_DECODE, 0xFF);
+  // Decode mode to "No Decode"
+  //MAX7219_writeData(MAX7219_MODE_DECODE, 0x00);
+  // Scan limit runs from 0.
+  writeData(MAX7219_MODE_SCAN_LIMIT, digitsInUse - 1);
+  writeData(MAX7219_MODE_INTENSITY, 8);
+  writeData(MAX7219_MODE_POWER, ON);
+	clearDisplay();
+}
+
---
+++ AvrCppBaseLib/Max7219.h
@@ -0,0 +1,32 @@
+/*
+ * max7219.h
+ *
+ * Created: 2018. 12. 23. 15:33:10
+ *  Author: Gergo Huszty
+ */ 
+
+
+#ifndef MAX7219_H_
+#define MAX7219_H_
+
+#include "SoftTimer.h"
+
+class Max7219: public SoftTimerHandler {
+  uint8_t digitsInUse;
+  uint8_t SsPin;
+  uint8_t refreshTime;
+  int32_t number;
+  int32_t prevNumber;
+  uint8_t dotPlace;
+  inline void spiSendByte(char);
+  inline void writeData(char, char);
+public:
+  Max7219(uint8_t newDigitsInUse, uint8_t newSsPin, uint8_t newRefreshTime);
+  void handleTimeout();
+  void init();
+  void applyContent();
+  void clearDisplay();
+  void setNumber(int32_t newNumber, uint8_t newDotPlace);
+};
+
+#endif /* MAX7219_H_ */
