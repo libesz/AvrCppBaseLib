242f160a8da32d218d5f69c1f3e8c36cd49aae9e
Gergo Huszty <gergo.huszty@digitaltrip.hu>
2019-02-01 23:39:33 +0100

    ShiftOut added to handle i.e. 74595 serial in parralel out shift register. It uses HW SPI with a latch pin

---
+++ AvrCppBaseLib/ShiftOut.h
@@ -0,0 +1,36 @@
+/* 
+* ShiftOut.h
+*
+* Created: 2019. 02. 01. 23:14:51
+* Author: Huszty Gergo
+*/
+
+
+#ifndef __SHIFTOUT_H__
+#define __SHIFTOUT_H__
+
+#include "CppStubs.h"
+
+template<uint8_t SsPin> class ShiftOut {
+	ShiftOut( const ShiftOut &c );
+	ShiftOut& operator=( const ShiftOut &c );
+  uint8_t data;
+public:
+	ShiftOut() {}
+	~ShiftOut() {}
+  void set(uint8_t index, bool state) {
+    if(state) {
+      data |= (1<<index);
+    } else {
+      data &= ~(1<<index);
+    }
+  }
+  void apply() {
+    SPDR = data;
+    while (!(SPSR & (1 << SPIF)));
+    PORTB |= (1<<SsPin);
+    PORTB &= ~(1<<SsPin);
+  }
+};
+
+#endif //__SHIFTOUT_H__
