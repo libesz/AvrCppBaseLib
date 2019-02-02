143ca331f0951a50125b03cd4ab6e72931acf233
Gergo Huszty <gergo.huszty@digitaltrip.hu>
2019-02-02 11:35:03 +0100

    ShiftIn added for handling 74165 parralel in, serial out shift register

---
+++ AvrCppBaseLib/ShiftIn.h
@@ -0,0 +1,32 @@
+/* 
+* ShiftIn.h
+*
+* Created: 2019. 02. 02. 11:14:39
+* Author: Huszty Gergo
+*/
+
+
+#ifndef __SHIFTIN_H__
+#define __SHIFTIN_H__
+
+#include "CppStubs.h"
+#include "avr/io.h"
+
+template<uint8_t ldPin> class ShiftIn {
+	ShiftIn( const ShiftIn &c );
+	ShiftIn& operator=( const ShiftIn &c );
+public:
+	ShiftIn() {
+    PORTB |= (1<<ldPin);
+  }
+	~ShiftIn();
+  uint8_t get() {
+    PORTB &= ~(1<<ldPin);
+    PORTB |= (1<<ldPin);
+    SPDR = 0;
+    while (!(SPSR & (1 << SPIF)));
+    return SPDR;
+  }
+};
+
+#endif //__SHIFTIN_H__
